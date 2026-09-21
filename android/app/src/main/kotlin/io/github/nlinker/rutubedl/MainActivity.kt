be2315d97bfa3f04284fd0ce196154521c8e86d4
Nick Linker <nlinker@gmail.com>
2026-09-21 14:06:22 +0600

    lang: update main activity and manifest

--- android/app/src/main/kotlin/io/github/nlinker/rutubedl/MainActivity.kt
+++ android/app/src/main/kotlin/io/github/nlinker/rutubedl/MainActivity.kt
@@ -2,14 +2,15 @@ package io.github.nlinker.rutubedl
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
-class MainActivity : ComponentActivity() {
+// AppCompatActivity, not ComponentActivity for language switch.
+class MainActivity : AppCompatActivity() {
     private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
